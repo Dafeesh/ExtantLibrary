d26a26cf4ae3e83e3cbf7646791981b0c8edca23
Scherschel <scherschb@leidos.com>
2016-07-01 18:14:35 -0400

    Added Color extensions to Extant.Unity. (I hate how C# handles structs)

---
+++ projects/Extant_Unity/ColorExtensions.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Extant.Unity
+{
+    public static class ColorExtensions
+    {
+        public static Color WithAlphaAs(this Color c, float a)
+        {
+            c.a = a;
+            return c;
+        }
+    }
+}
